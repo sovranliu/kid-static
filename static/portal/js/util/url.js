ef8bfb929f236d47ec5817989c5042e4b7e75f36
xuyidan <evaxyd@gmail.com>
2017-07-30 23:27:21 +0800

    初始化

---
+++ static/portal/js/util/url.js
@@ -0,0 +1,28 @@
+define([], function() {
+    var _basePath = 'http://121.40.90.141:8003/kid/wechat/'; //todo 发布时改成生产地址
+
+    return {
+        getTicketPrice: _basePath + 'getTicketPrice',
+        getTickets: _basePath + 'getTickets',
+        buyFlightDiary: _basePath + 'buyFlightDiary',
+        submitBooking: _basePath + 'submitBooking',
+        getBookingTime: _basePath + 'getBookingTime',
+        getBookableNum: _basePath + 'getBookableNum',
+        buyTicket: _basePath + 'buyTicket',
+        getFlightDiary: _basePath + 'getFlightDiary',
+        //编辑用户信息
+        getUserInfo: _basePath + 'getUserInfo',
+        postUserInfo: _basePath + 'postUserInfo',
+        //留言
+        getMessageData: _basePath + 'getMessageData',
+        postMessageData: _basePath + 'postUserInfo',
+        //会员权益
+        postBenefit: _basePath + 'postUserInfo',
+        //预约相关
+        getUserBooks: _basePath + 'getBooks',
+        //退款相关
+        getRefund: _basePath + 'getRefund',
+        postRefund: _basePath + 'postUserInfo'
+
+    };
+});
