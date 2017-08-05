b14bdc5dcd749fa79b71bda31601538a0d7a9684
Elaine.Wang <747584854@qq.com>
2017-08-06 00:00:22 +0800

    update

--- static/wechat/js/biz/myorder.js
+++ static/wechat/js/biz/myorder.js
@@ -10,6 +10,7 @@ define(['mustache','url', 'helper','handshake'], function(Mustache,url, helper,h
         $('.js-ticket-share').on('click', _shareTicket);
         $('.js-send-message').on('click', _sendMessage);
         $('.js-confirm-message-result').on('click', _confirmMessgeResult);
+        $('.js-revoke').on('click', _handleRevoke);
         $('.revoke-popup').on('click','.js-confirm',function() {
             $('.revoke-popup').hide();
             window.location.reload();
@@ -198,7 +199,23 @@ define(['mustache','url', 'helper','handshake'], function(Mustache,url, helper,h
     }
 
     function _shareTicket() {
+        wx.onMenuShareAppMessage({
+            title: '', // 分享标题
+            desc: '', // 分享描述
+            link: '', // 分享链接，该链接域名或路径必须与当前页面对应的公众号JS安全域名一致
+            imgUrl: '', // 分享图标
+            type: '', // 分享类型,music、video或link，不填默认为link
+            dataUrl: '', // 如果type是music或video，则要提供数据链接，默认为空
+            success: function () { 
+                // 用户确认分享后执行的回调函数
+            },
+            cancel: function () { 
+                // 用户取消分享后执行的回调函数
+            }
+        });
+    }
 
+    function _handleRevoke() {
         helper.ajax(url.getTickets, params, function(res) {
             if(res.code == 0) {
                 $('.revoke-popup').find('p').html('撤销成功，请至我的飞行票查看。');
