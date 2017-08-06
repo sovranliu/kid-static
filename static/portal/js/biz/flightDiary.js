a0010a85ff2e9e0b7e561f28beee2d65e427df1c
xuyidan <evaxyd@gmail.com>
2017-08-06 20:50:19 +0800

    update

--- static/portal/js/biz/flightDiary.js
+++ static/portal/js/biz/flightDiary.js
@@ -10,7 +10,7 @@ define(['url', 'helper', 'mustache'], function (url, helper, mustache) {
         helper.ajax(url.getFlightDiary, params, function(res) {
             var data = res.data;
             if(res.code >= 0) {
-                if (data.hasPurchased.length == 0) {
+                if (!data || data.hasPurchased.length == 0) {
                     $('.js-video-list').html('<p class="dataNull">您还没有飞行礼品</p>');
                 } else {
                     $('.js-video-list').html(mustache.render($('#videoTmpl').html(), { 'videoList': data.hasPurchased }));
