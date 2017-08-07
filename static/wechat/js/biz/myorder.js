e26ba64db9eae392aed0375a13f89f7e267462c4
Elaine.Wang <747584854@qq.com>
2017-08-08 00:18:42 +0800

    update

--- static/wechat/js/biz/myorder.js
+++ static/wechat/js/biz/myorder.js
@@ -278,7 +278,7 @@ define(['mustache','url', 'helper','handshake','wechat'], function(Mustache,url,
                             title: '赠送飞行票', // 分享标题  
                             desc: '赠送飞行票给我的朋友', // 分享描述  
                             link: 'http://solution.slfuture.cn/kid/static/wechat/ReceiveTicket.html?serialNumber=' + serialNumber, // 分享链接  
-                            imgUrl: 'http://solution.slfuture.cn/kid/static/wechat/images/logo.png', // 分享图标  
+                            imgUrl: 'http://solution.slfuture.cn/kid/static/wechat/images/error.png', // 分享图标  
                             type: 'link', // 分享类型,music、video或link，不填默认为link  
                             dataUrl: '', // 如果type是music或video，则要提供数据链接，默认为空  
                             success: function () {   
