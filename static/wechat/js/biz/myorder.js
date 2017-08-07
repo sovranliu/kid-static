abf2be9fe8894e874e896f5ff4298667427eaa16
Elaine.Wang <747584854@qq.com>
2017-08-07 23:35:20 +0800

    update

--- static/wechat/js/biz/myorder.js
+++ static/wechat/js/biz/myorder.js
@@ -217,11 +217,6 @@ define(['mustache','url', 'helper','handshake','wechat'], function(Mustache,url,
         $('.send-message-result').hide();
     }
 
-    function _shareTicket(){
-        
-        
-    }
-
     function onBridgeReady(data) {
         alert('start share');
         WeixinJSBridge.on('menu:share:appmessage', function(argv) {  
@@ -250,7 +245,7 @@ define(['mustache','url', 'helper','handshake','wechat'], function(Mustache,url,
 
             if(res.code >= 0) {
 
-                if (typeof WeixinJSBridge == "undefined") {
+                /*if (typeof WeixinJSBridge == "undefined") {
                    if (document.addEventListener) {
                        document.addEventListener('WeixinJSBridgeReady', onBridgeReady, false);
                    } else if (document.attachEvent) {
@@ -259,7 +254,48 @@ define(['mustache','url', 'helper','handshake','wechat'], function(Mustache,url,
                    }
                 } else {
                     onBridgeReady(data);
-                }
+                }*/
+
+                alert('wx config');
+                wx.config({  
+                    debug: true, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。  
+                    appId: data.appId, // 必填，公众号的唯一标识  
+                    timestamp: data.timestamp, // 必填，生成签名的时间戳  
+                    nonceStr: data.nonceStr, // 必填，生成签名的随机串  
+                    signature: data.signature,// 必填，签名  
+                    jsApiList: [  
+                        'checkJsApi',  
+                        'onMenuShareTimeline',  
+                        'onMenuShareAppMessage' 
+                    ]
+                });  
+              
+                wx.ready(function () {  
+                    alert('wx ready');
+                    wx.onMenuShareAppMessage({ 
+                        title: '赠送飞行票', // 分享标题  
+                        desc: '赠送飞行票给我的朋友', // 分享描述  
+                        link: window.location.origin + '/kid/static/wechat/ReceiveTicket.html?serialNumber=' + serialNumber, // 分享链接  
+                        imgUrl: window.location.origin + '/kid/static/wechat/images/logo.png', // 分享图标  
+                        type: 'link', // 分享类型,music、video或link，不填默认为link  
+                        dataUrl: '', // 如果type是music或video，则要提供数据链接，默认为空  
+                        success: function () {   
+                             $('.send-message-result').show();
+                             $('.js-send-message-result').html('<p style="text-align:center;">分享成功</p>');
+                        },  
+                        cancel: function () {   
+                            // 用户取消分享后执行的回调函数  
+                        }  
+                    });   
+                    wx.error(function(res){  
+                        alert('wx error');
+                        // config信息验证失败会执行error函数，如签名过期导致验证失败，具体错误信息可以打开config的debug模式查看，也可以在返回的res参数中查看，对于SPA可以在这里更新签名。  
+                        $('.send-message-result').show();
+                        $('.js-send-message-result').html('错误信息：' + res); 
+                    });  
+                });
+
+
             }
         })
     }
