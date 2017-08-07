41aa06470fcd667352f2428f9145f7abdf75f4aa
Elaine.Wang <747584854@qq.com>
2017-08-08 00:15:55 +0800

    update\

--- static/wechat/js/biz/myorder.js
+++ static/wechat/js/biz/myorder.js
@@ -264,29 +264,32 @@ define(['mustache','url', 'helper','handshake','wechat'], function(Mustache,url,
                     nonceStr: data.nonceStr, // 必填，生成签名的随机串  
                     signature: data.signature,// 必填，签名  
                     jsApiList: [  
-                        'checkJsApi',  
+                        //'checkJsApi',  
                         //'onMenuShareTimeline',  
                         'onMenuShareAppMessage' 
                     ]
                 });  
               
                 wx.ready(function () {  
-                    alert('http://solution.slfuture.cn//kid/static/wechat/ReceiveTicket.html?serialNumber=' + serialNumber);
-                    wx.onMenuShareAppMessage({ 
-                        title: '赠送飞行票', // 分享标题  
-                        desc: '赠送飞行票给我的朋友', // 分享描述  
-                        link: 'http://solution.slfuture.cn//kid/static/wechat/ReceiveTicket.html?serialNumber=' + serialNumber, // 分享链接  
-                        imgUrl: 'http://solution.slfuture.cn//kid/static/wechat/images/logo.png', // 分享图标  
-                        type: 'link', // 分享类型,music、video或link，不填默认为link  
-                        dataUrl: '', // 如果type是music或video，则要提供数据链接，默认为空  
-                        success: function () {   
-                             $('.send-message-result').show();
-                             $('.js-send-message-result').html('<p style="text-align:center;">分享成功</p>');
-                        },  
-                        cancel: function () {   
-                            // 用户取消分享后执行的回调函数  
-                        }  
-                    });   
+                    alert('http://solution.slfuture.cn/kid/static/wechat/ReceiveTicket.html?serialNumber=' + serialNumber);
+                    
+                    setTimeout(function() {
+                        wx.onMenuShareAppMessage({ 
+                            title: '赠送飞行票', // 分享标题  
+                            desc: '赠送飞行票给我的朋友', // 分享描述  
+                            link: 'http://solution.slfuture.cn/kid/static/wechat/ReceiveTicket.html?serialNumber=' + serialNumber, // 分享链接  
+                            imgUrl: 'http://solution.slfuture.cn/kid/static/wechat/images/logo.png', // 分享图标  
+                            type: 'link', // 分享类型,music、video或link，不填默认为link  
+                            dataUrl: '', // 如果type是music或video，则要提供数据链接，默认为空  
+                            success: function () {   
+                                 $('.send-message-result').show();
+                                 $('.js-send-message-result').html('<p style="text-align:center;">分享成功</p>');
+                            },  
+                            cancel: function () {   
+                                // 用户取消分享后执行的回调函数  
+                            }  
+                        });
+                    }, 2000);
                     wx.error(function(res){  
                         alert('wx error');
                         // config信息验证失败会执行error函数，如签名过期导致验证失败，具体错误信息可以打开config的debug模式查看，也可以在返回的res参数中查看，对于SPA可以在这里更新签名。  
