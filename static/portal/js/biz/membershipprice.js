7272aa63e97328cf7735010a09ada86deef9328f
xuyidan <evaxyd@gmail.com>
2017-08-13 14:26:34 +0800

    update

--- static/portal/js/biz/membershipprice.js
+++ static/portal/js/biz/membershipprice.js
@@ -2,7 +2,7 @@ define(['mustache','url', 'helper'], function(Mustache,url, helper) {
 
     function _getMaterielData() {
         var params = {
-            "type": 'preference'
+            "type": 1
         }
 
         helper.ajax(url.getMateriel, params, function(res) {
