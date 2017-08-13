e0c1ba241befde42b61979592993948249f63e0a
Elaine.Wang <747584854@qq.com>
2017-08-13 09:33:27 +0800

    update

--- static/wechat/js/biz/buyTickets.js
+++ static/wechat/js/biz/buyTickets.js
@@ -7,8 +7,8 @@ define(['url', 'helper','handshake'], function (url, helper,handshake) {
             var data = res.data;
 
             if (res.code >= 0) {
-                $('.js-single-price').text(data.single / 100);
-                $('.js-group-price').text(data.group / 100); 
+                $('.js-single-price').text((Number(data.single) / 100).toFixed(2));
+                $('.js-group-price').text((Number(data.group) / 100).toFixed(2)); 
             }
         });
     }
